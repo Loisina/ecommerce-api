41f6d44d4d0f1c4bf1927f9ac02d6dffa1a4ec8c
loisina <adjoaasiedu4@gmail.com>
2025-03-05 15:24:39 +0000

    saved to db and fetched

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,27 @@
-export const addProducts = (req, res) => {
-  //upload product image
-  // validate product info
-  // save product info in database
-  // return response
-  res.json(req.body);
+import { ProductModel } from "../models/product.js";
+
+export const addProducts = async (req, res, next) => {
+ try {
+   //upload product image
+   // validate product info
+   // save product info in database
+   const result = await ProductModel.create(req.body)
+   // return response
+   res.json(result);
+ } catch (error) {
+    next(error);
+ }
 }
 
-export const getProducts = (req, res) => {
-  
-  res.send('All Products!');
+export const getProducts = async (req, res, next) => {
+  try {
+    // fetch products from database
+    const result = await ProductModel.find();
+    // return response
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export const countProducts = (req, res) => {
